ee5fed30d06a0b0e6f9555494031c3150e1cfe8a
Stinth <malthejelstrup@gmail.com>
2022-10-11 02:48:59 +0200

    plotting

--- Assignments/2-ODTMS/part_1.jl
+++ Assignments/2-ODTMS/part_1.jl
@@ -3,6 +3,7 @@ using Gurobi
 using Formatting
 import XLSX
 include("help_files/distanceCalculation.jl")
+include("plot_part_1.jl")
 
 xf = XLSX.readxlsx("DataProject2.xlsx")
 sheet = xf["Sheet1"]
@@ -18,7 +19,6 @@ bike_speed = sheet["H2"]
 
 
 
-# Upper matrix only
 connections = Bool.(sheet["B23:R39"])
 
 # volume of passengers that would like to travel from a origin (row) to a destination
@@ -26,7 +26,7 @@ connections = Bool.(sheet["B23:R39"])
 OD = sheet["B47:R63"]
 
 
-budget = 100000
+budget = 500
 
 # distance matrix between all station_positions
 distances = zeros(size(station_positions, 1), size(station_positions, 1))
@@ -49,44 +49,16 @@ for (i, start) in enumerate(eachrow(station_positions))
     end
 end
 
-function is_path_possible(check_connections, bus_connections, bike_connections, origin, destination, visited = [])
-    # push!(visited, origin)
-    for (i, connection) in enumerate(check_connections[origin, :]) # swapping
-        # println("checking connection $i, from $origin to $destination")
-        if connection == 1 && !(i in visited)
-            push!(visited, i)
-            if i == destination
-                println("found something")
-                return 1
-            else
-                # check if last node can be reached by bike
-                # this is needed because busses can not drive between node 7 and 8, but bikes can
-                for (j, connection) in enumerate(bike_connections[i, :]) # bikes
-                    if connection == 1 && !(j in visited) && j == destination
-                        println("found something")
-                        return 1
-                    end
-                end
-                # println("i: $i, connection: $connection")
-                if is_path_possible(bus_connections, bus_connections, bike_connections, i, destination, visited) # bus?
-                    println("found something")
-                    return 1
-                end
-            end
-        end
-    end
-    return 0
-end
-
 model = Model(Gurobi.Optimizer)
-# set_optimizer_attribute(model, "TimeLimit", 100)
-# set_optimizer_attribute(model, "Presolve", 0)
 
 # enabled bus connections
 @variable(model, x[1:N, 1:N], Bin)
 
-# enabled bike connections
+# enabled bike connections as start or end node
 @variable(model, y[1:N, 1:N], Bin)
+
+# enabled direct bike connections
+@variable(model, q[1:N, 1:N], Bin)
 
 # 1 if travler from i to j is using connection k to l, otherwise 0 
 @variable(model, z[1:N, 1:N, 1:N, 1:N], Bin)
@@ -95,23 +67,41 @@ model = Model(Gurobi.Optimizer)
 # minimize the total user inconvenience; distance traveled by bike and distance traveled by bus
 # bus travels twice as fast as bike
 @objective(model, Min, 
-    sum((distance_cost[k,l] .* x[k,l]/bus_speed + distance_cost[k,l] .* y[k,l]/bike_speed) .* z[i,j,k,l] for i in 1:N, j in 1:N, k in 1:N, l in 1:N))
+    sum((OD[i,j] .* distances[k,l]/bus_speed .* x[k,l] + OD[i,j] .* distances[k,l]/bike_speed .* y[k,l] + OD[i,j] .* distances[k,l]/bike_speed .* q[k,l]) .* z[i,j,k,l] for i in 1:N, j in 1:N, k in 1:N, l in 1:N))
+# @objective(model, Min, 
+#     sum((distance_cost[k,l] .* x[k,l]/bus_speed + distance_cost[k,l] .* y[k,l]/bike_speed) .* z[i,j,k,l] for i in 1:N, j in 1:N, k in 1:N, l in 1:N))
 
 # crazy constraint fixes everything, thank you
 # demand -1 at origin, demand +1 at destination, 0 in between
 # balance constraint, what comes into a stop has to go out again (unless origin/destination)
-@constraint(model, [i=1:N,j=1:N,k=1:N],
-            sum(z[i,j,k,l] * (x[k,l]+y[k,l]) for l=1:N) + (i==k ? -1 : 0) + (j==k ? 1 : 0) == sum(z[i,j,l,k] * (x[l,k]+y[l,k]) for l=1:N))
+# @constraint(model, [i=1:N,j=1:N,k=1:N],
+#             sum(z[i,j,k,l] * (x[k,l]+
+#             ((bike_connections[k,l] && (i==k || j==l)) ? y[k,l] : 0) +
+#             ((i==k && j==l) ? q[k,l] : 0)) for l=1:N) + (i==k ? -1 : 0) + (j==k ? 1 : 0) == 
+#             sum(z[i,j,l,k] * (x[l,k] +
+#             ((bike_connections[l,k] && (i==l || j==k)) ? y[l,k] : 0) +
+#             (i==l && j==k ? q[l,k] : 0)) for l=1:N))
+@constraint(model, [i=1:N, j=1:N, k=1:N],
+            sum(z[i,j,k,l] * (x[k,l]+ ((bike_connections[k,l] && (i==k || j==l)) ? y[k,l] : 0) + ((i==k && j==l) ? q[k,l] : 0)) for l=1:N) + (i==k ? -1 : 0) + (j==k ? 1 : 0) == 
+            sum(z[i,j,l,k] * (x[l,k] + ((bike_connections[l,k] && (i==l || j==k)) ? y[l,k] : 0)) + ((i==l && j==k) ? q[l,k] : 0) for l=1:N))
+
+# @constraint(model, [i=1:N,j=1:N,k=1:N], sum(z[i,j,k,l] * ((i==k && j==l) ? q[k,l] : 0) for l=1:N) == sum(z[i,j,k,l] * ((i==l && j==k) ? q[l,k] : 0) for l=1:N))
+
+            # +
+            # ((i==k && j==l) ? q[k,l] : 0) = (i==l && j==k ? q[l,k] : 0)
 
 # budget constraint can get optimal of 0 with budget of 800
-@constraint(model, sum(x .* distances) <= 400)
+@constraint(model, sum(x .* distances) <= budget)
+# @constraint(model, x .== connections)
 
 # a path can only be used if it is enabled
 @constraint(model, x .<= connections)
 @constraint(model, y .<= bike_connections)
+@constraint(model, q .<= 1)
+@constraint(model, [i=1:N], q[i,i] == 0)
 
 # a path can only be used for either bike or bus
-@constraint(model, x + y .<= 1)
+@constraint(model, x + y + q .<= 1)
 
 
 optimize!(model)
@@ -147,6 +137,41 @@ if termination_status(model) == MOI.OPTIMAL
 else
     println("No optimal solution found.")
 end
+travel = sum(value.(z)[i,j,:,:] for i in 1:N, j in 1:N)
+plot_solution(value.(x), value.(y), value.(q), travel, station_positions, station_id, distances, budget, objective_value(model))
 
 
+function find_path(z,origin,destination)
+    z_ = value.(z)
+    path = [origin]
+    while path[end] != destination
+        for (i, val) in enumerate(z_[origin, destination, path[end], :])
+            if val == 1
+                push!(path, i)
+                break
+            end
+        end
+    end
+    print(path)
+end
 
+# path_find(z, 12, 15)
+
+x_cost = sum(value.(x) .* (distance_cost/bus_speed) .* travel)
+y_cost = sum(value.(y) .* distance_cost/bike_speed .* travel)
+q_cost = sum(value.(q) .* distance_cost/bike_speed .* travel)
+# show objective contribution
+println("-------------------------------------")
+println("Objective contribution:")
+println("Bus: $(x_cost)")
+println("Bike: $(y_cost)")
+println("Direct bike: $(q_cost)")
+println("Total: $(x_cost + y_cost + q_cost)")
+println("objective_value: $(objective_value(model))")
+
+
+# @objective(model, Min, 
+#     sum((distance_cost[k,l]/bus_speed .* x[k,l] + distance_cost[k,l]/bike_speed .* y[k,l] + distance_cost[k,l]/bike_speed .* q[k,l]) .* z[i,j,k,l] for i in 1:N, j in 1:N, k in 1:N, l in 1:N))
+
+
+# (distance_cost[3,4] + distance_cost[4,2]) * OD[3,2] - distance_cost[3,2] * OD[3,2]
---
+++ Assignments/2-ODTMS/plot_part_1.jl
@@ -0,0 +1,184 @@
+using Plots
+using Images
+
+
+# arrow0!(map_nodes[2][1], map_nodes[2][2],(map_nodes[1][1]-map_nodes[2][1])*0.98,(map_nodes[1][2]-map_nodes[2][2])*0.98, as=0.07, width=4, lc=:red, la=5)
+# arrow0!(map_nodes[1][1], map_nodes[1][2],(map_nodes[2][1]-map_nodes[1][1])*0.98,(map_nodes[2][2]-map_nodes[1][2])*0.98, as=0.07, width=4, lc=:red, la=5)
+function plot_solution(x_, y_, q_, travel, station_positions, station_id, distances, budget, objective_value)
+    function arrow0!(x, y, u, v; as=0.07, width=3, lc=:red, la=5)  # by @rafael.guerra
+        if as < 0
+            quiver!([x],[y],quiver=([u],[v]), lc=lc, la=la)  # NB: better use quiver directly in vectorial mode
+        else
+            nuv = sqrt(u^2 + v^2)
+            v1, v2 = [u;v] / nuv,  [-v;u] / nuv
+            v4 = (3*v1 + v2)/3.1623  # sqrt(10) to get unit vector
+            v5 = v4 - 2*(v4'*v2)*v2
+            v4, v5 = as*nuv*v4, as*nuv*v5
+            # plot outline
+            real_u, real_v = copy(u), copy(v)
+            u,v = u*0.7, v*0.7
+            # plot!([x,x+real_u], [y,y+real_v], lc=:black,la=la, width=width+2)
+            # plot!([x+u,x+u-v5[1]], [y+v,y+v-v5[2]], lc=:black, la=la, width=width+2)
+            # plot!([x+u,x+u-v4[1]], [y+v,y+v-v4[2]], lc=:black, la=la, width=width+2)
+            # plot arrow
+            plot!([x,x+real_u], [y,y+real_v], lc=lc,la=la, width=width, primary=false)
+            plot!([x+u,x+u-v5[1]], [y+v,y+v-v5[2]], lc=lc, la=la, width=width, primary=false)
+            plot!([x+u,x+u-v4[1]], [y+v,y+v-v4[2]], lc=lc, la=la, width=width, primary=false)
+        end
+    end
+
+    function convert(x, y)
+        x_1 = 3.76
+        x_2 = 663
+        y_1 = -3.82
+        y_2 = 1268
+        return x*x_1 + x_2, y*y_1 + y_2
+    end 
+    
+    map_nodes = convert.(station_positions[:,1], station_positions[:,2])
+    
+    img = Images.load("map3_clean.png")
+    w = 1192
+    h = 805
+    # plot image
+    plot(img, xlims = (0, w), ylims = (0, h), axis=([], false), size=(w, h))
+    # plot bike connections
+    for (i, start) in enumerate(map_nodes)
+        for (j, stop) in enumerate(map_nodes)
+            if i > j 
+                if (q_[i, j] == 1 && travel[i, j] > 0) || (q_[j, i] == 1 && travel[j, i] > 0)
+                    # create outline for both directions
+                    if q_[i, j] == 1
+                        arrow0!(start[1], start[2], (stop[1]-start[1]), (stop[2]-start[2]), as=0.05, width=7, lc=:black, la=5)
+                    end
+                    if q_[j, i] == 1
+                        arrow0!(stop[1], stop[2], (start[1]-stop[1]), (start[2]-stop[2]), as=0.05, width=7, lc=:black, la=5)
+                    end
+                    
+
+                    # create arrow for both directions
+                    if q_[i, j] == 1
+                        arrow0!(start[1], start[2], (stop[1]-start[1]), (stop[2]-start[2]), as=0.05, width=5, lc=:turquoise, la=5)
+                    end
+                    if q_[j, i] == 1
+                        arrow0!(stop[1], stop[2], (start[1]-stop[1]), (start[2]-stop[2]), as=0.05, width=5, lc=:turquoise, la=5)
+                    end
+                end
+            end
+        end
+    end
+
+    # #plot bike connecitons arrow
+    # for (i, start) in enumerate(map_nodes)
+    #     for (j, stop) in enumerate(map_nodes)
+    #         if y_[i, j] == 1
+    #             d = 1#(distances[i,j] / maximum(distances)) * 0.15 + 0.85
+    #             arrow0!(start[1], start[2], (stop[1]-start[1])*d, (stop[2]-start[2])*d, as=0.08, width=5, lc=:dodgerblue, la=5)
+    #             # plot!([start[1],stop[1]], [start[2],stop[2]], color=:dodgerblue , linewidth=3) #dodgerblue
+    #         end
+    #     end
+    # end
+    # plot bike connections
+    for (i, start) in enumerate(map_nodes)
+        for (j, stop) in enumerate(map_nodes)
+            if i > j 
+                if (y_[i, j] == 1 && travel[i, j] > 0) || (y_[j, i] == 1 && travel[j, i] > 0)
+                    # create outline for both directions
+                    if y_[i, j] == 1
+                        arrow0!(start[1], start[2], (stop[1]-start[1]), (stop[2]-start[2]), as=0.08, width=7, lc=:black, la=5)
+                    end
+                    if y_[j, i] == 1
+                        arrow0!(stop[1], stop[2], (start[1]-stop[1]), (start[2]-stop[2]), as=0.08, width=7, lc=:black, la=5)
+                    end
+                    
+
+                    # create arrow for both directions
+                    if y_[i, j] == 1
+                        arrow0!(start[1], start[2], (stop[1]-start[1]), (stop[2]-start[2]), as=0.08, width=5, lc=:dodgerblue, la=5)
+                    end
+                    if y_[j, i] == 1
+                        arrow0!(stop[1], stop[2], (start[1]-stop[1]), (start[2]-stop[2]), as=0.08, width=5, lc=:dodgerblue, la=5)
+                    end
+                end
+            end
+        end
+    end
+    # for (i, start) in enumerate(map_nodes)
+    #     for (j, stop) in enumerate(map_nodes)
+    #         if i > j 
+    #             if y_[i, j] == 1 && y_[j, i] == 1
+    #                 # create outline for both directions
+    #                 arrow0!(start[1], start[2], (stop[1]-start[1]), (stop[2]-start[2]), as=0.08, width=7, lc=:black, la=5)
+    #                 # arrow0!(start[2], start[1], (stop[2]-start[2]), (stop[1]-start[1]), as=0.08, width=7, lc=:black, la=5)
+
+    #                 # create arrow for both directions
+    #                 arrow0!(start[1], start[2], (stop[1]-start[1]), (stop[2]-start[2]), as=0.08, width=5, lc=:dodgerblue, la=5)
+    #                 # arrow0!(start[2], start[1], (stop[2]-start[2]), (stop[1]-start[1]), as=0.08, width=5, lc=:dodgerblue, la=5)
+
+    #             end
+    #         end
+    #     end
+    # end
+    # plot bus connections arrow
+    for (i, start) in enumerate(map_nodes)
+        for (j, stop) in enumerate(map_nodes)
+            if i > j 
+                if (x_[i, j] == 1 && travel[i, j] > 0) || (x_[j, i] == 1 && travel[j, i] > 0)
+                    # create outline for both directions
+                    if x_[i, j] == 1
+                        arrow0!(start[1], start[2], (stop[1]-start[1]), (stop[2]-start[2]), as=0.1, width=7, lc=:black, la=5)
+                    end
+                    if x_[j, i] == 1
+                        arrow0!(stop[1], stop[2], (start[1]-stop[1]), (start[2]-stop[2]), as=0.1, width=7, lc=:black, la=5)
+                    end
+                    
+
+                    # create arrow for both directions
+                    if x_[i, j] == 1
+                        arrow0!(start[1], start[2], (stop[1]-start[1]), (stop[2]-start[2]), as=0.1, width=5, lc=:gold1, la=5)
+                    end
+                    if x_[j, i] == 1
+                        arrow0!(stop[1], stop[2], (start[1]-stop[1]), (start[2]-stop[2]), as=0.1, width=5, lc=:gold1, la=5)
+                    end
+                end
+            end
+        end
+    end
+    # plot stations
+    scatter!(map_nodes, label="Stations", color=:red, markersize=18)
+    # annotate station ids
+    for ((x,y),id) in zip(map_nodes, station_id)
+        annotate!(x, y, text(id, :black, 12))
+    end
+    
+    annotate!(30,40, text("Budget: $(budget)", :black, 22, :left))
+    annotate!(30,95, text("Cost: $(sum(x_ .* distances))", :black, 22, :left))
+    annotate!(30,150, text("Objective: $(objective_value)", :black, 22, :left))
+
+    plot!([0, 0], [0, 0], color=:gold1, linewidth=0, label="Bus Connection")
+    plot!([0, 0], [0, 0], color=:dodgerblue, linewidth=0, label="Bike Last Leg")
+    plot!([0, 0], [0, 0], color=:turquoise, linewidth=0, label="Bike Direct")
+    plot!()
+    savefig("part1_budget_$(budget).png")
+end
+
+# plot(img, xlims = (0, w), ylims = (0, h), axis=([], false), legend=:none, size=(w, h))
+
+# # 1
+# scatter!((610,89), label="A", color=:red, markersize=16)
+# # 7
+# scatter!((834,541), label="A", color=:red, markersize=16)
+# # 12
+# scatter!((155,669), label="A", color=:red, markersize=16)
+# function convert(x, y)
+#     x_1 = 3.76
+#     x_2 = 663
+#     y_1 = -3.82
+#     y_2 = 1268
+#     return x*x_1 + x_2, y*y_1 + y_2
+# end 
+# convert(46.7, 191.1)
+
+# station_positions
+# map_nodes = convert.(station_positions[:,1], station_positions[:,2])
+# scatter!(map_nodes, label="A", color=:red, markersize=18)
