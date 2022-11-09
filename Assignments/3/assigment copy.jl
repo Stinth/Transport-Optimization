58e7f602fb3ffbb36c605dea7e88433f750184a1
Stinth <malthejelstrup@gmail.com>
2022-11-09 17:00:56 +0100

    working state, still issues

--- Assignments/3/assigment copy.jl
+++ Assignments/3/assigment copy.jl
@@ -11,10 +11,14 @@ types = readTrainTypesFromCSV("types.csv")
 
 stations = ["Amf", "Gn", "Gvc", "Lls", "Lw", "Rtd", "Shl", "Ut", "Zl"]
 S = length(stations)
-ordered_stations = reverse(stations)#["Amf", "Gn", "Gvc", "Lls", "Lw", "Rtd", "Shl", "Ut", "Zl"]
 station_dict = Dict{String, Int64}()
 for i in 1:S
-    station_dict[ordered_stations[i]] = i
+    station_dict[stations[i]] = i
+end
+ordered_stations = reverse(stations)#["Amf", "Gn", "Gvc", "Lls", "Lw", "Rtd", "Shl", "Ut", "Zl"]
+reverse_station_dict = Dict{String, Int64}()
+for i in 1:S
+    reverse_station_dict[ordered_stations[i]] = i
 end
 
 arcs = []
@@ -68,7 +72,7 @@ for number in unique(train_numbers)
     end
     if first_departure[1] != "t"
         println("s to ", first_departure)
-        push!(P_s_, (number, ("s", Time(0,00)), first_departure, 0, 0, 50))
+        push!(P_s_, (number, ("s", Time(0,00)), (first_departure[2], first_departure[3]), 0, 0, 50))
     end
 end
 arcs = sort(union(P_e_, P_between_, P_s_, arcs))
@@ -76,18 +80,25 @@ sort!(arcs, by=x -> (x[1], x[2][2]))
 arc_train_numbers = [arc[1] for arc in arcs] 
 start_arcs = []
 stop_arcs = []
-for arc in arcs
-    if arc[2][1] == "s"
-        push!(start_arcs, 1)
-    else
-        push!(start_arcs, 0)
+for station in stations
+    start_station = []
+    stop_station = []
+    for arc in arcs
+        if arc[2][1] == "s" && arc[3][1] == station
+            push!(start_station, 1)
+        else
+            push!(start_station, 0)
+        end
+        if arc[3][1] == "t" && arc[2][1] == station
+            push!(stop_station, 1)
+        else
+            push!(stop_station, 0)
+        end
     end
-    if arc[3][1] == "t"
-        push!(stop_arcs, 1)
-    else
-        push!(stop_arcs, 0)
-    end
+    push!(start_arcs, start_station)
+    push!(stop_arcs, stop_station)
 end
+start_arcs_sum = sum(start_arcs[s] for s = 1:S)
 
 
 alphas = Vector{Int}[]
@@ -155,8 +166,8 @@ T = 2
 P = length(alphas[1,:])
 P = 2
 init_paths = []
-push!(init_paths, alphas[:,3:4])
-push!(init_paths, zeros(Int, I, P))
+push!(init_paths, alphas[:,1:5])
+push!(init_paths, alphas[:,1:5])
 P = length(init_paths[1][1,:])
 function MRP(init_paths)
     P = length(init_paths[1][1,:])
@@ -177,8 +188,8 @@ function MRP(init_paths)
     # second class
     @constraint(model, second_class[i=1:I], sum(S_s_t[t] * sum(init_paths[t][i,p] * y[p,t] for p = 1:P) for t in 1:2) + delta_s[i] >= arcs[i][5])
     # number of trains at the start and end stations must be equal
-    @constraint(model, station[t=1:T, i=1:I], L_t[t] * sum(init_paths[t][i,p] * start_arcs[i] * y[p,t] for p = 1:P) - L_t[t] * sum(init_paths[t][i,p] * stop_arcs[i] * y[p,t] for p = 1:P) == 0)
-
+    @constraint(model, station[t=1:T, s=1:S], L_t[t] * sum(init_paths[t][i,p] * start_arcs[s][i] * y[p,t] for p = 1:P, i = 1:I) - L_t[t] * sum(init_paths[t][i,p] * stop_arcs[s][i] * y[p,t] for p = 1:P, i = 1:I) == 0)
+    # @constraint(model, y[1,1] == 1) something is wrong here, any value of y is out of bounds probably last constraint
     optimize!(model)
     println("Objective value: ", objective_value(model))
     π = dual.(max_length)
@@ -187,7 +198,7 @@ function MRP(init_paths)
     μ = dual.(station)
     return π, λ_f, λ_s, μ
 end
-
+fake_stations = [ "Amf", "Gn", "Gvc", "Lls", "Lw", "Rtd", "Shl", "Ut", "Zl", "s", "t"]
 function solve_pricing(π::Vector{Float64}, λ_f::Vector{Float64}, λ_s::Vector{Float64}, μ::Matrix{Float64})
     I = length(π)
     model = Model(Gurobi.Optimizer)
@@ -195,16 +206,28 @@ function solve_pricing(π::Vector{Float64}, λ_f::Vector{Float64}, λ_s::Vector{
     @variable(model, y[1:I,1:T], Bin)
     # arc capacity
     # @constraint(model, [i=1:I], sum(L_t[t] * sum(alphas[i,p] * y[p,t] for p = 1:P) for t in 1:2) <= arcs[i][6])
-    @objective(model, Max, sum(sum(C_t[t] * y[a,t] for t = 1:T, a=1:I) - sum((π[a] * L_t[t] + arcs[a][t+3] * λ_f[a] + arcs[a][t+4] * λ_s[a]) .* y[a,t] for t = 1:T, a=1:I) .- sum((start_arcs[a] .* μ'[a,t]) .* y[a,t] for a = 1:I, t=1:T) + sum((stop_arcs[a] .* μ'[a,t]) .* y[a,t] for a = 1:I, t=1:T)))
+    @objective(model, Max, sum(sum(C_t[t] * y[a,t] for t = 1:T, a=1:I) - sum((π[a] * L_t[t] + arcs[a][t+3] * λ_f[a] + arcs[a][t+4] * λ_s[a]) .* y[a,t] for t = 1:T, a=1:I) .- sum(((arcs[a][2][1] == "s" ? start_arcs[station_dict[arcs[a][3][1]]][a] * μ[t,station_dict[arcs[a][3][1]]] : 0)) * y[a,t] for a = 1:I, t=1:T) + sum(((arcs[a][3][1] == "t" ? stop_arcs[station_dict[arcs[a][2][1]]][a] * μ[t,station_dict[arcs[a][2][1]]] : 0)) * y[a,t] for a = 1:I, t=1:T)))
     # artificial arcs from s must sum to 1
-    @constraint(model, [t=1:T], sum(y[a,t]*start_arcs[a] for a = 1:I) == 1)
+    @constraint(model, [t=1:T], sum(y[a,t]*start_arcs[s][a] for a = 1:I, s=1:S) == 1)
     # artificial arcs to t must sum to 1
-    @constraint(model, [t=1:T], sum(y[a,t]*stop_arcs[a] for a = 1:I) == 1)
+    @constraint(model, [t=1:T], sum(y[a,t]*stop_arcs[s][a] for a = 1:I, s=1:S) == 1)
     # the train number must be the same for all active arcs per type
-    # @constraint(model, [a=1:I-1,t=1:T], (y[a,t] == 1 ? arcs[a][3] : 0) == (arcs[a][3] == arcs[a+1][3] ? arcs[a+1][3] : ("t", Time(0))))
+    # @constraint(model, flow[a=1:I-1,t=1:T], y[a,t] <= y[a,t] * y[a+1,t] * (arcs[a][3][1] != "t" ? 1 : 0))
+    # @constraint(model, flow[t=1:T,s=1:S], sum((arcs[a][2][1] == stations[s] ? y[a,t] : 0) + start_arcs[s][a] .* y[a,t] for a = 1:I) == sum((arcs[a][3][1] == stations[s] ? y[a,t] : 0) + stop_arcs[s][a] .* y[a,t] for a = 1:I)) 
+    # @constraint(model, flow[t=1:T,i=1:I], sum(arc_train_numbers[a] * start_arcs[s][a] * y[a,t] for a = 1:I,s=1:S) == arc_train_numbers[i] * y[i,t])
     # demand -1 at origin, demand +1 at destination, 0 in between
-    @constraint(model, [a=1:I, t=1:T], sum((start_arcs .* arc_train_numbers .* y[:,t])) * y[a,t] == arc_train_numbers[a] * y[a,t])
-
+    # @constraint(model, flow[a=1:I, t=1:T], sum(sum(start_arcs[s] for s=1:S) .* arc_train_numbers .* y[:,t]) * y[a,t] == arc_train_numbers[a] * y[a,t])
+    # @constraint(model, flow[a=1:I, t=1:T], (y[a,t] == 1 ? arc_train_numbers[a] : 0) == (y[a,t] == 1 ? arc_train_numbers[a] : 0))
+    # @constraint(model, [a=1:I, t=1:T], sum((sum(start_arcs[s] for s=1:S) .* arc_train_numbers .* y[:,t])) * y[a,t] == arc_train_numbers[a] * y[a,t])
+    # @constraint(model, [t=1:T], sum((arc_train_numbers[a] == sum((sum(start_arcs[s] for s=1:S) .* arc_train_numbers .* y[:,t])) * y[a,t] ? 1 : 0) for a in 1:I) == sum(y[:,t]))
+    for t = 1:T
+        for a = 2:I
+            if start_arcs_sum[a] == 0
+                @constraint(model, y[a-1,t] == y[a,t])
+            end
+        end
+    end
+    # @constraint(model, y[1,1] == y[2,1])
     optimize!(model)
     if objective_value(model) > 1
         return round.(Int, value.(y))
@@ -212,14 +235,44 @@ function solve_pricing(π::Vector{Float64}, λ_f::Vector{Float64}, λ_s::Vector{
     return nothing
     println("test")
 end
-π, λ_f, λ_s, μ = MRP(init_paths)
+# π, λ_f, λ_s, μ = MRP(init_paths)
+# print(sum(π))
 new_pattern = solve_pricing(π, λ_f, λ_s, μ)
 
 init_paths[1] = hcat(init_paths[1], new_pattern[:,1])
 init_paths[2] = hcat(init_paths[2], new_pattern[:,2])
 # I don't know how to get this to work from the template 
-push!(y, @variable(model, lower_bound = 0))
+# push!(y, @variable(model, lower_bound = 0))
 # we need to add 2 variables the the decision variable y
+y = vcat(y, Vector([@variable(model, lower_bound = 0),@variable(model, lower_bound = 0)])')
+set_objective_coefficient(model, y[Int(length(y)/2)], 1.0)
+set_objective_coefficient(model, y[end], 1.0)
+
+for t in 1:T
+    for i in 1:I
+        if new_pattern[i,t] > 0
+            # max length
+            set_normalized_coefficient(max_length[i], y[end], new_pattern[i,t])
+            set_normalized_coefficient(max_length[i], y[Int(length(y)/2)], new_pattern[i,t])
+
+            # first class
+            set_normalized_coefficient(first_class[i], y[end], new_pattern[i,t])
+            set_normalized_coefficient(first_class[i], y[Int(length(y)/2)], new_pattern[i,t])
+
+            # second class
+            set_normalized_coefficient(second_class[i], y[end], new_pattern[i,t])
+            set_normalized_coefficient(second_class[i], y[Int(length(y)/2)], new_pattern[i,t])
+        end
+    end
+    for s in 1:S
+        # station
+        set_normalized_coefficient(station[t, s], y[end], 1)
+        set_normalized_coefficient(station[t, s], y[Int(length(y)/2)], 1)
+    end
+end
+
+# station[t=1:T, s=1:S]
+
 
 
 all_nodes = []
@@ -293,7 +346,7 @@ for i in 1:S
     nlist[i+1] = []
 end
 for (i, node) in enumerate(unique_nodes)
-    push!(nlist[station_dict[node[1]]+1], i)
+    push!(nlist[reverse_station_dict[node[1]]+1], i)
 end
 reverse!(nlist)
 locs_x, locs_y = GraphPlot.shell_layout(g, nlist)
@@ -342,3 +395,10 @@ end
 # # objective
 # @objective(model, Min, sum(C_t[t] * sum(y[p, t] for p in 1:Graphs.ne(g)) for t in 1:length(types))
 #                     + sum(C_p * (d_f[i, j] + d_s[i, j]) for i in 1:Graphs.nv(g), j in 1:Graphs.nv(g)))
+
+
+for arc in arcs
+    if arc[2][1] == stations[6] && arc[3][1] == "t"
+        println(arc)
+    end
+end
